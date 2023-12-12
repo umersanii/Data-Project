5870eff7af2a0655419b281e160949d72ad2cd3b
umersanii <iamumersani@gmail.com>
2023-12-13 00:43:57 +0500

    0.3
    BTree Code from around the world
    Needs further checkup

--- Data Project/main.cpp
+++ Data Project/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Classes.h"
 #include "HelpingFunctions.h"
+#include "BTree.h"
 
 
 using std::cout;
@@ -9,8 +10,58 @@ using std::endl;
 using std::string;
 int main()
 {
+    BTree t(3);
+    t.insertion(8);
+    t.insertion(9);
+    t.insertion(10);
+    t.insertion(11);
+    t.insertion(15);
+    t.insertion(16);
+    t.insertion(17);
+    t.insertion(18);
+    t.insertion(20);
+    t.insertion(23);
+
+    cout << "The B-tree is: ";
+    t.traverse();
+
+    int k = 10;
+    (t.search(k) != NULL) ? cout << endl
+        << k << " is found"
+        : cout << endl
+        << k << " is not Found";
+
+    k = 2;
+    (t.search(k) != NULL) ? cout << endl
+        << k << " is found"
+        : cout << endl
+        << k << " is not Found\n";
+
+    /*
+    BTree t(3);
+    t.insertion(8);
+    t.insertion(9);
+    t.insertion(10);
+    t.insertion(11);
+    t.insertion(15);
+    t.insertion(16);
+    t.insertion(17);
+    t.insertion(18);
+    t.insertion(20);
+    t.insertion(23);
+
+    cout << "The B-tree is: ";
+    t.traverse();
+
+    t.deletion(20);
+
+    cout << "\nThe B-tree is: ";
+    t.traverse();
+    */
 	//cout << "Rom Rom";
-	cout << CalculateHash("Hello", 100);
+	cout << CalculateHash("Hello", 100)<<"\n";
+    cout << CalculateHash("Hello", 100);
+
 	return 0;
 
 	CircularLinkedList<string> list;
@@ -22,3 +73,5 @@ int main()
 	list.print();
 	return 0;
 }
+
+
